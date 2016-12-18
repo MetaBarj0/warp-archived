404d2b03ae0ca246cb54abf9cec056cbf1cb1da5
MetaBarj0 <troctsch.cpp@gmail.com>
2016-12-18 21:02:55 +0100

    spark
    - created files that will contain implementation details about a graph
      representation for spark regular grammars.

---
+++ src/warp/spark/detail/automaton.hpp
@@ -0,0 +1,20 @@
+#ifndef _WARP_SPARK_DETAIL_AUTOMATON_HPP_
+#define _WARP_SPARK_DETAIL_AUTOMATON_HPP_
+
+/**
+ * \brief A namespace containing implementation details. Elements inside this
+ * namespace are not intended to be directly used in the client side
+ */
+namespace warp::spark::detail
+{
+}
+
+#endif // _WARP_SPARK_DETAIL_AUTOMATON_HPP_
+
+// doxygen
+/**
+ * \file
+ *
+ * \brief This file contain the definition of a deterministic finite automaton,
+ * graph representation of a spark regular grammar
+ */
---
+++ src/warp/spark/detail/automaton_traits.hpp
@@ -0,0 +1,15 @@
+#ifndef _WARP_SPARK_DETAIL_AUTOMATON_TRAITS_HPP_
+#define _WARP_SPARK_DETAIL_AUTOMATON_TRAITS_HPP_
+
+namespace warp::spark::detail
+{
+}
+
+#endif // _WARP_SPARK_DETAIL_AUTOMATON_TRAITS_HPP_
+
+// doxygen
+/**
+ * \file
+ *
+ * \brief This file contains the definition of traits for an automaton type
+ */
